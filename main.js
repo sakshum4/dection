9479d07dd857b8f46aa374e5563501bb57c458af
sakshum4 <85113220+sakshum4@users.noreply.github.com>
2021-06-28 17:42:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
 
 img = "";
+status = "";
 
 
 
@@ -22,6 +23,8 @@ function setup()
 {
 canvas = createCanvas(640, 420);
 canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "status : Detecting Objects";
 }
 
 
@@ -36,4 +39,33 @@ text("dog", 45, 75);
 noFill();
 stroke("#FF0000");
 rect(30, 60, 450, 350 );
+
+fill("FF0000");
+text("cat" ,320 ,120 );
+noFill();
+stroke("FF0000");
+rect(300, 90, 270, 320)
+}
+
+
+
+
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+
+
+
+
+function gotResults(error, results) 
+{
+if (error) {
+    console.log(error);
+}
+console.log(results);
 }
