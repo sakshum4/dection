3f8c273c157827da0293200a5bfae7f9795a4ec2
sakshum4 <85113220+sakshum4@users.noreply.github.com>
2021-06-29 17:56:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
 
 img = "";
 status = "";
+objects = [];
 
 
 
@@ -32,20 +33,18 @@ document.getElementById("status").innerHTML = "status : Detecting Objects";
 
 
 function draw()
-{
-    image(img, 0, 0, 640, 420);
-fill("#FF0000");
-text("dog", 45, 75);
-noFill();
-stroke("#FF0000");
-rect(30, 60, 450, 350 );
-
-fill("FF0000");
-text("cat" ,320 ,120 );
-noFill();
-stroke("FF0000");
-rect(300, 90, 270, 320)
-}
+ { image(img, 0, 0, 640, 420);
+     if(status != "")
+      { for (i = 0; i < objects.length; i++)
+         { document.getElementById("status").innerHTML = "Status : Object Detected";
+          fill("#FF0000");
+           percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+             noFill(); stroke("#FF0000");
+              rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+             }
+             }
+             }
 
 
 
@@ -68,4 +67,5 @@ if (error) {
     console.log(error);
 }
 console.log(results);
+objects = results;
 }
